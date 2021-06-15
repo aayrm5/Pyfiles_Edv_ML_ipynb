32581464cb63afe4b5e2c198a97f0c250cba87cd
Riyaz Ali <54612827+aayrm5@users.noreply.github.com>
2021-06-15 13:49:37 +0530

    test commit

---
+++ __Riz__/Java/Float_and_Double/src/Float_Double.java
@@ -0,0 +1,14 @@
+public class Float_Double {
+    public static void main(String[] args){
+        
+        float myMinFloatVal=Float.MIN_VALUE;
+        float myMaxFloatVal=Float.MAX_VALUE;
+        System.out.println("Min Float Value ="+myMinFloatVal);
+        System.out.println("Max Float Value ="+myMaxFloatVal);
+
+        double myMinDoubleVal=Double.MIN_VALUE;
+        double myMaxDoubleVal=Double.MAX_VALUE;
+        System.out.println("Min Double Value ="+myMinDoubleVal);
+        System.out.println("Max Double Value ="+myMaxDoubleVal);
+    }
+}
